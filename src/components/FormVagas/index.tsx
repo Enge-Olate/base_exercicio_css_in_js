06d2a4c627daeacf7d2b46af7922ca31c00536af
@Enge-Olate <marcioalexisolate@live.com>
2025-10-05 15:53:57 -0300

    Estilizando FormVagas.

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useState } from 'react'
 
-import styles from './FormVagas.module.css'
+import { Botao, FormStyle, Input } from './FormStyle'
 
 type Props = {
   aoPesquisar: (termo: string) => void
@@ -15,17 +15,14 @@ const FormVagas = ({ aoPesquisar }: Props) => {
   }
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <FormStyle>
+      <Input
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
-        Pesquisar
-      </button>
-    </form>
+      <Botao type="submit">Pesquisar</Botao>
+    </FormStyle>
   )
 }
 export default FormVagas
