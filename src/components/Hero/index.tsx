3a232362777fcdac7ac4c925f0fb8331c5b81929
Enge-Olate <marcioalexisolate@live.com>
2025-10-06 10:16:26 -0300

    Estilizando globalmente.

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,12 @@
-import styles from './Hero.module.css'
-
+import { FormHero, HeroTitle } from './Herostyles'
 const Hero = () => (
-  <form className={styles.form}>
+  <FormHero>
     <div className="container">
-      <h2 className={styles.heroTitle}>
+      <HeroTitle>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
+      </HeroTitle>
     </div>
-  </form>
+  </FormHero>
 )
 
 export default Hero
